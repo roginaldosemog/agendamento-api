76c9430d5644940aef1577c0e4cb7f0fca9a7f8b
Igor Aragão <roginaldosemog@gmail.com>
2021-03-24 17:59:24 -0300

    Change port choice to variable

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+const port = process.env.PORT || '8080';
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
